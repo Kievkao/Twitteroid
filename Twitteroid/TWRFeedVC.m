3fc05572559ea710e54902f755305c739ba5bc38
Andrey Kravchenko <kievkao@gmail.com>
2015-07-24 12:57:09 +0300

    Added demo behavior for PullToRefresh && InfinitiveScroll

--- Twitteroid/TWRFeedVC.m
+++ Twitteroid/TWRFeedVC.m
@@ -9,8 +9,15 @@
 #import "TWRFeedVC.h"
 #import "TWRTwitCell.h"
 #import "TWRTwitterAPIManager+TWRFeed.h"
+#import "UIScrollView+INSPullToRefresh.h"
+#import "INSAnimatable.h"
+#import "INSTwitterPullToRefresh.h"
+#import "INSPullToRefreshBackgroundView.h"
+#import "INSCircleInfiniteIndicator.h"
 
 @interface TWRFeedVC ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -19,13 +26,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [[TWRTwitterAPIManager sharedInstance] getFeedSinceTwitID:nil count:20 completion:^(NSError *error, NSArray *items) {
-        NSLog(@"hello");
-    }];
+    [self pullToRefreshSetup];
+    [self infinitiveScrollSetup];
+    
+//    [[TWRTwitterAPIManager sharedInstance] getFeedSinceTwitID:nil count:20 completion:^(NSError *error, NSArray *items) {
+//        NSLog(@"hello");
+//    }];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     self.navigationController.navigationBarHidden = NO;
+}
+
+#pragma mark - Infinitive scroll && PullToRefresh
+- (void)pullToRefreshSetup {
+    [self.tableView ins_addPullToRefreshWithHeight:60.0 handler:^(UIScrollView *scrollView) {
+        int64_t delayInSeconds = 1;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [scrollView ins_endPullToRefresh];
+        });
+    }];
+    
+    UIView <INSPullToRefreshBackgroundViewDelegate> *pullToRefresh = [self pullToRefreshViewFromCurrentStyle];
+    self.tableView.ins_pullToRefreshBackgroundView.delegate = pullToRefresh;
+    [self.tableView.ins_pullToRefreshBackgroundView addSubview:pullToRefresh];
+}
+
+- (void)infinitiveScrollSetup {
+    [self.tableView ins_addInfinityScrollWithHeight:60 handler:^(UIScrollView *scrollView) {
+        
+        int64_t delayInSeconds = 1;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [scrollView ins_endInfinityScrollWithStoppingContentOffset:YES];
+        });
+    }];
+    
+    UIView <INSAnimatable> *infinityIndicator = [self infinityIndicatorViewFromCurrentStyle];
+    [self.tableView.ins_infiniteScrollBackgroundView addSubview:infinityIndicator];
+    [infinityIndicator startAnimating];
+}
+
+- (UIView <INSAnimatable> *)infinityIndicatorViewFromCurrentStyle {
+    return [[INSCircleInfiniteIndicator alloc] initWithFrame:CGRectMake(0, 0, 24, 24)];
+}
+
+- (UIView <INSPullToRefreshBackgroundViewDelegate> *)pullToRefreshViewFromCurrentStyle {
+    return [[INSTwitterPullToRefresh alloc] initWithFrame:CGRectMake(0, 0, 24, 24)];
 }
 
 #pragma mark - UITableView Datasource
